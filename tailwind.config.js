64364cd3ed0a45e20584ebdac9076a7a77ff94af
Jeyker-Dev <134793631+J0K3R1178@users.noreply.github.com>
2024-06-11 10:39:02 -0400

    Agregado Subtitulo junto con la lista de tiempo de preparacion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: 
+    {
+      fontFamily: 
+      {
+        "oswald": ['Oswald', 'sans-serif'],
+        "source-code": ['Source Code Pro', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
